c3251a14eb85c1f5f99eed71e05c84d7a2a62e45
kekeing <851575436@qq.com>
2019-08-23 16:41:07 +0800

    should_get_fizzbuzz_when_fizzBuzzTest_then_15

--- src/main/java/com/thoughtworks/tdd/fizzBuzzNumberGame.java
+++ src/main/java/com/thoughtworks/tdd/fizzBuzzNumberGame.java
@@ -3,15 +3,23 @@ package com.thoughtworks.tdd;
 public class fizzBuzzNumberGame {
 
     public String fizzBuzz(int number) {
+          String result = "";
+    	
         if(number % 3 == 0) {
-        	return "fizz";
+        	 result += "fizz";
         }
         if(number % 5 == 0) {
-        	return "buzz";
+        	 result += "buzz";
         }
         if(number % 7 == 0) {
         	return "whizz";
         }
-        else return Integer.toString(number);
+        if (number % 3 != 0  &&  number % 5 != 0) {
+        	 result = Integer.toString(number);
+			
+		} 
+        
+        
+        return result;
     }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -48,7 +48,7 @@ public class FizzBuzzTest {
 
     }
     @Test
-    public void should_get_Whizz_when_fizzBuzzTest_then_5(){
+    public void should_get_Whizz_when_fizzBuzzTest_then_7(){
         //given
         int number = 7;
         fizzBuzzNumberGame fizzBuzzNumberGame = new fizzBuzzNumberGame();
@@ -59,6 +59,20 @@ public class FizzBuzzTest {
 
         //then
         Assertions.assertEquals("whizz" , word);
+
+    }
+    @Test
+    public void should_get_fizzbuzz_when_fizzBuzzTest_then_15(){
+        //given
+        int number = 15;
+        fizzBuzzNumberGame fizzBuzzNumberGame = new fizzBuzzNumberGame();
+
+
+        //when
+        String word = fizzBuzzNumberGame.fizzBuzz(number);
+
+        //then
+        Assertions.assertEquals("fizzbuzz" , word);
 
     }
 }
