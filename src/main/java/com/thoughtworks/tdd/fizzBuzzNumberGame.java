217ca6ec58494d95ee665b61c229da5b9fdae89f
kekeing <851575436@qq.com>
2019-08-23 16:32:41 +0800

    should_get_Fizz_when_fizzBuzzTest_then_3

--- src/main/java/com/thoughtworks/tdd/fizzBuzzNumberGame.java
+++ src/main/java/com/thoughtworks/tdd/fizzBuzzNumberGame.java
@@ -3,6 +3,15 @@ package com.thoughtworks.tdd;
 public class fizzBuzzNumberGame {
 
     public String fizzBuzz(int number) {
-        return "1";
+        if(number % 3 == 0) {
+        	return "fizz";
+        }
+        if(number % 5 == 0) {
+        	return "buzz";
+        }
+        if(number % 7 == 0) {
+        	return "whizz";
+        }
+        else return Integer.toString(number);
     }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void should_get1_when_fizzBuzzTest_then_1(){
+    public void should_get_1_when_fizzBuzzTest_then_1(){
         //given
         int number = 1;
         fizzBuzzNumberGame fizzBuzzNumberGame = new fizzBuzzNumberGame();
@@ -17,6 +17,48 @@ public class FizzBuzzTest {
 
         //then
         Assertions.assertEquals("1" , word);
+
+    }
+    @Test
+    public void should_get_Fizz_when_fizzBuzzTest_then_3(){
+        //given
+        int number = 3;
+        fizzBuzzNumberGame fizzBuzzNumberGame = new fizzBuzzNumberGame();
+
+
+        //when
+        String word = fizzBuzzNumberGame.fizzBuzz(number);
+
+        //then
+        Assertions.assertEquals("fizz" , word);
+
+    }
+    @Test
+    public void should_get_Buzz_when_fizzBuzzTest_then_5(){
+        //given
+        int number = 5;
+        fizzBuzzNumberGame fizzBuzzNumberGame = new fizzBuzzNumberGame();
+
+
+        //when
+        String word = fizzBuzzNumberGame.fizzBuzz(number);
+
+        //then
+        Assertions.assertEquals("buzz" , word);
+
+    }
+    @Test
+    public void should_get_Whizz_when_fizzBuzzTest_then_5(){
+        //given
+        int number = 7;
+        fizzBuzzNumberGame fizzBuzzNumberGame = new fizzBuzzNumberGame();
+
+
+        //when
+        String word = fizzBuzzNumberGame.fizzBuzz(number);
+
+        //then
+        Assertions.assertEquals("whizz" , word);
 
     }
 }
